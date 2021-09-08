59151ac595d38006c4f3592138ddca8f46d1d5b4
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-08 14:00:27 +0200

    finish iteration 2 bug fixes

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,52 @@ const router = require('express').Router();
 const parser = require('../middleware/file-upload');
 const Post = require('../models/Post.model');
 
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Post.find()
+    .then((posts) => res.render('index', { posts }))
+    .catch((e) => next(e));
 });
 
 router.get('/create-post', (req, res) => {
   res.render('post-form');
 });
 
-router.post('/create-post', parser.single('picture'), (req, res) => {
-  const { content } = req.body;
-  const picture = req.file.path || null;
-
-  Post.create({
-    content,
-    creator: req.session.user._id,
-    picName: req.file.originalname,
-    picPath: picture
-  })
+router.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  Post.findById(id)
+    .populate('creator')
     .then((post) => {
       console.log(post);
-      res.redirect('/');
+      res.render('post-detail', { post });
     })
     .catch((e) => next(e));
 });
+
+router.post(
+  '/create-post',
+  isLoggedIn,
+  parser.single('picture'),
+  (req, res) => {
+    const { content } = req.body;
+    const picture = req.file.path || null;
+    const user = req.session.user._id;
+    console.log(user);
+
+    Post.create({
+      content,
+      creator: user,
+      picName: req.file.originalname,
+      picPath: picture
+    })
+      .then((post) => {
+        console.log(post);
+        res.redirect('/');
+      })
+      .catch((e) => next(e));
+  }
+);
 
 module.exports = router;
