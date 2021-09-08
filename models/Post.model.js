9b744b4a13a742c30723ba45c5786b74983aca72
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-08 12:25:51 +0200

    solve iteration 1

---
+++ models/Post.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    content: String,
+    creatorId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    picPath: String,
+    picName: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
+
+router.get('/create-post', (req, res) => {
+  res.render('post-form');
+});
+
+router.post('/create-post', (req, res) => {});
 
 module.exports = router;
